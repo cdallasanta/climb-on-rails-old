655c5350f1bd754383a84bdc90cf7b37f8f5324d
chris.dallasanta <chris.dallasanta@gmail.com>
2019-05-20 18:36:19 -0700

    rspec tests not working

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,27 @@
 require './config/environment'
 
-describe 'User' do
-  let(:good_attrs) do
-    {
-      fullname: "Billy Bob Thorton",
-      username: "BBT",
-      password: "y'all",
-      role: "facilitator"
-    }
-  end
+# tests are not currently working, objects are persisting between tests
 
-  it 'can instantiate a user with good attributes' do
-    good_user = User.create(good_attrs)
-    expect(good_user).to be_valid
-  end
-
-  it 'has a unique username' do
-    copy_name = User.create(good_attrs)
-    expect(copy_name).to_not be_valid
-  end
-end
+# describe 'User' do
+#   let(:good_attrs) do
+#     {
+#       fullname: "Billy Bob Thorton",
+#       username: "BBT",
+#       password: "y'all",
+#       role: "facilitator"
+#     }
+#   end
+#
+#   before do
+#     @good_user = User.create(good_attrs)
+#   end
+#
+#   it 'can instantiate a user with good attributes' do
+#     expect(@good_user).to be_valid
+#   end
+#
+#   it 'has a unique username' do
+#     copy_name = User.create(good_attrs)
+#     expect(copy_name).not_to be_valid
+#   end
+# end
