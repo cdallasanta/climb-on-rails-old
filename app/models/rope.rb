3e6d6dae1c9c9fc6a3f2c6a99ab7211401c8ee37
chris.dallasanta <chris.dallasanta@gmail.com>
2019-06-08 22:19:08 -0700

    added seeds

--- app/models/rope.rb
+++ app/models/rope.rb
@@ -6,7 +6,7 @@ class Rope < ApplicationRecord
   validates_presence_of :identifier
 
   # returns the number of climbs this rope has received in its life
-  # note: not the number of Climb object, but the number of uses
+  # note: not the number of Climb objects, but the number of uses
   # TODO, there's probably a better method, maybe something like #sum or #total that
   # can accept a block? Or #collect?
   def climb_count
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Site
+orkila = Site.create(name:"Orkila")
+
+#Elements
+catwalk = Element.create(name:"Catwalk", site: orkila)
+zipline = Element.create(name:"Zipline", site: orkila)
+giants_ladder = Element.create(name:"Giant's Ladder", site: orkila)
+
+#rope
+Rope.create(element:catwalk, identifier:"Olive with red pcord")
+Rope.create(element:catwalk, identifier:"Blue with red pcord")
+Rope.create(element:zipline, identifier:"North")
+Rope.create(element:zipline, identifier:"South")
+Rope.create(element:giants_ladder, identifier:"Red with orange pcord")
+Rope.create(element:giants_ladder, identifier:"White with orange pcord")
+Rope.create(element:giants_ladder, identifier:"Blue with orange pcord")
+
+#Users
+chris = User.create(
+  fullname: "Chris Dalla Santa",
+  email: "chris@email.com",
+  password: "p",
+  site: orkila,
+  role: "admin"
+)
+liz = User.create(
+  fullname: "Liz Doane",
+  email: "liz@email.com",
+  password: "p",
+  site: orkila,
+  role: "lead"
+)
+emily = User.create(
+  fullname: "Emily Trusky",
+  email: "emily@email.com",
+  password: "p",
+  site: orkila,
+  role: "facilitator"
+)
