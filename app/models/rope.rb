6440ed489ad00286153e843f1801f104af2dd021
chris.dallasanta <chris.dallasanta@gmail.com>
2019-06-08 22:24:47 -0700

    made a better #climb_count

--- app/models/rope.rb
+++ app/models/rope.rb
@@ -7,11 +7,7 @@ class Rope < ApplicationRecord
 
   # returns the number of climbs this rope has received in its life
   # note: not the number of Climb objects, but the number of uses
-  # TODO, there's probably a better method, maybe something like #sum or #total that
-  # can accept a block? Or #collect?
   def climb_count
-    sum = 0
-    self.climbs.each { |climb| sum += climb.number_of_climbs}
-    sum
+    self.climbs.inject(0){|sum, climb| sum + climb.number_of_climbs }
   end
 end
