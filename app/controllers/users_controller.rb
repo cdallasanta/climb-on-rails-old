e9b6549b37a2e3c43b0c22c357e3314da9f55980
chris.dallasanta <chris.dallasanta@gmail.com>
2019-06-11 13:30:37 -0700

    basic login working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
-  def login
+  def new
+    render 'login'
+  end
+
+  def create
+    user_params = params[:user]
+    user = User.find_by(email: user_params[:email])
+    # binding.pry
+    if user && user.authenticate(user_params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      render 'login'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'elements#index'
 
-  get '/login', to: 'users#login'
+  get '/login', to: 'users#new'
+  post '/users', to: 'users#create'
 end
