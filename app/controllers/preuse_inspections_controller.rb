a7131c680802dbaacb76a5bfa18d650b053be152
chris.dallasanta <chris.dallasanta@gmail.com>
2019-06-11 23:05:10 -0700

    working on preuse #update

--- app/controllers/preuse_inspections_controller.rb
+++ app/controllers/preuse_inspections_controller.rb
@@ -29,5 +29,34 @@ class PreuseInspectionsController < ApplicationController
   end
 
   def update
+    binding.pry
+    #updating preuse (just the date, really)
+    preuse = PreuseInspection.find_by(id: params[:id])
+    preuse.date = preuse_params[:date]
+    #save preuse for date validation
+    unless preuse.save
+      render element_preuse_inspection_path(preuse.element, preuse)
+    end
+
+    #updating setup
+    setup = preuse.setup
+    setup.update(preuse_params[:preuse_inspection_setup])
+    setup.users << current_user unless setup.users.include?(current_user)
+
+    #updating takedown
+    #more to come
+
+    #TODO flash message for success? also check for other errors, like form editing?
+    redirect_to element_preuse_inspection_path(preuse.element, preuse)
+  end
+
+  private
+
+  def preuse_params
+    params.require(:preuse_inspection).permit(
+      :date,
+      preuse_inspection_setup:
+        [:equipment_complete, :element_complete, :environment_complete]
+      )
   end
 end
