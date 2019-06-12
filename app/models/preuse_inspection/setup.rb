5d9dc756684fd89adc57a468a2f788e077a7644e
chris.dallasanta <chris.dallasanta@gmail.com>
2019-06-11 22:29:23 -0700

    fixed user_setup join table

--- app/models/preuse_inspection/setup.rb
+++ app/models/preuse_inspection/setup.rb
@@ -1,6 +1,7 @@
 class PreuseInspection::Setup < ApplicationRecord
   self.table_name = "setups"
   belongs_to :preuse_inspection
+  has_many :user_setups, class_name: "JoinTable::UserSetups"
   has_many :users, through: :user_setups
   has_many :comments, as: :commentable
   has_one :element, through: :preuse_inspection
--- app/models/preuse_inspection/takedown.rb
+++ app/models/preuse_inspection/takedown.rb
@@ -1,6 +1,7 @@
 class PreuseInspection::Takedown < ApplicationRecord
   self.table_name = "takedowns"
   belongs_to :preuse_inspection
+  has_many :user_takedowns, class_name: "JoinTable::UserTakedowns"
   has_many :users, through: :user_takedowns
   has_many :climbs, class_name: "Element::Rope::Climb"
   has_many :ropes, through: :preuse_inspection
