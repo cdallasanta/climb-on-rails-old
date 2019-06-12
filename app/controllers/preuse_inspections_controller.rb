6de878042eb233b8a4a2c000b8e782f5488e0732
chris.dallasanta <chris.dallasanta@gmail.com>
2019-06-12 12:55:36 -0700

    changes from lunch

--- app/controllers/preuse_inspections_controller.rb
+++ app/controllers/preuse_inspections_controller.rb
@@ -6,16 +6,20 @@ class PreuseInspectionsController < ApplicationController
     @inspection.setup = PreuseInspection::Setup.create if @inspection.setup == nil
 
     if @inspection.valid?
-      redirect_to element_preuse_inspection_path(@inspection.element, @inspection)
+      redirect_to edit_element_preuse_inspection_path(@inspection.element, @inspection)
     else
       flash[:alert] = "Element not found"
       render elements_path
     end
   end
 
-  # currently, no #new is needed, TODO remove route from config
 
   def show
+    #TODO is this used?
+  end
+  # currently, no #new is needed, TODO remove route from config
+
+  def edit
     @inspection = PreuseInspection.find_by(id: params[:id])
     @element = @inspection.element
     #TODO check for url schenanegains for both element and inspection
@@ -23,9 +27,6 @@ class PreuseInspectionsController < ApplicationController
     if @inspection.setup.is_complete?
       @inspection.takedown ||= PreuseInspection::Takedown.create
     end
-  end
-
-  def edit
   end
 
   def update
