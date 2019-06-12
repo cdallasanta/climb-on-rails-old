230085103d05c60e005c13cf7b3d5c4dbf9ec392
chris.dallasanta <chris.dallasanta@gmail.com>
2019-06-11 22:54:06 -0700

    setup form displaying correctly

--- app/models/element.rb
+++ app/models/element.rb
@@ -7,18 +7,19 @@ class Element < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :site
 
-  after_initialize :default_instructions
+  after_create :default_instructions
 
   #TODO figure out how to deal with default instructions.
 
   def default_instructions
     #TODO, this is filler until I work on elements/new
-    preuse_equipment_instructions ||= "preuse_equipment_instructions filler"
-    preuse_element_instructions ||= "preuse_element_instructions filler"
-    preuse_environment_instructions ||= "preuse_environment_instructions filler"
-    periodic_equipment_instructions ||= "periodic_equipment_instructions filler"
-    periodic_element_instructions ||= "periodic_element_instructions filler"
-    periodic_environment_instructions ||= "periodic_environment_instructions filler"
+    self.preuse_equipment_instructions ||= "preuse_equipment_instructions filler"
+    self.preuse_element_instructions ||= "preuse_element_instructions filler"
+    self.preuse_environment_instructions ||= "preuse_environment_instructions filler"
+    self.periodic_equipment_instructions ||= "periodic_equipment_instructions filler"
+    self.periodic_element_instructions ||= "periodic_element_instructions filler"
+    self.periodic_environment_instructions ||= "periodic_environment_instructions filler"
+    self.save
    end
 
   def date_of_last_periodic
