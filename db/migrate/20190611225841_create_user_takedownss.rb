80227b05f22917dfe9701617edbe190c7d2447e9
chris.dallasanta <chris.dallasanta@gmail.com>
2019-06-11 22:13:25 -0700

    small typo fixes

--- db/migrate/20190611225841_create_user_takedownss.rb
+++ db/migrate/20190611225841_create_user_takedownss.rb
@@ -1,6 +1,6 @@
-class CreateUserTakedownss < ActiveRecord::Migration[5.2]
+class CreateUserTakedowns < ActiveRecord::Migration[5.2]
   def change
-    create_table :user_takedownsses do |t|
+    create_table :user_takedowns do |t|
       t.integer :user_id
       t.integer :takedown_id
     end
