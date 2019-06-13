ac136072a68e807bf05c3e03489dff97b9a8b4a4
chris.dallasanta <chris.dallasanta@gmail.com>
2019-06-13 16:24:57 -0700

    default values tweaked

--- app/models/element.rb
+++ app/models/element.rb
@@ -10,15 +10,29 @@ class Element < ApplicationRecord
   after_create :default_instructions
 
   #TODO figure out how to deal with default instructions.
+  @@DEFAULT_INSRUCTIONS = {
+    setup_equipment_instructions: "All gear checked<br>Bad gear set aside",
+    setup_element_instructions: "Everything appears normal<br>Backup halos not engaged",
+    setup_environment_instructions: "Weather is appropriate for climbing<br>No widowmakers",
+    takedown_equipment_instructions: "Gear stored properly<br>Helmets sprayed",
+    takedown_element_instructions: "Access methods removed",
+    takedown_environment_instructions: "Area clear of garbage",
+    periodic_equipment_instructions: "filler",
+    periodic_element_instructions: "filler",
+    periodic_environment_instructions: "filler"
+  }
 
   def default_instructions
     #TODO, this is filler until I work on elements/new
-    self.preuse_equipment_instructions ||= "preuse_equipment_instructions filler"
-    self.preuse_element_instructions ||= "preuse_element_instructions filler"
-    self.preuse_environment_instructions ||= "preuse_environment_instructions filler"
-    self.periodic_equipment_instructions ||= "periodic_equipment_instructions filler"
-    self.periodic_element_instructions ||= "periodic_element_instructions filler"
-    self.periodic_environment_instructions ||= "periodic_environment_instructions filler"
+    self.setup_equipment_instructions ||= @@DEFAULT_INSRUCTIONS[:setup_equipment_instructions]
+    self.setup_element_instructions ||= @@DEFAULT_INSRUCTIONS[:setup_element_instructions]
+    self.setup_environment_instructions ||= @@DEFAULT_INSRUCTIONS[:setup_environment_instructions]
+    self.takedown_equipment_instructions ||= @@DEFAULT_INSRUCTIONS[:takedown_equipment_instructions]
+    self.takedown_element_instructions ||= @@DEFAULT_INSRUCTIONS[:takedown_element_instructions]
+    self.takedown_environment_instructions ||= @@DEFAULT_INSRUCTIONS[:takedown_environment_instructions]
+    self.periodic_equipment_instructions ||= @@DEFAULT_INSRUCTIONS[:periodic_equipment_instructions]
+    self.periodic_element_instructions ||= @@DEFAULT_INSRUCTIONS[:periodic_element_instructions]
+    self.periodic_environment_instructions ||= @@DEFAULT_INSRUCTIONS[:periodic_environment_instructions]
     self.save
    end
 
--- db/migrate/20190609040155_create_elements.rb
+++ db/migrate/20190609040155_create_elements.rb
@@ -3,9 +3,12 @@ class CreateElements < ActiveRecord::Migration[5.2]
     create_table :elements do |t|
       t.string :name
       t.integer :site_id
-      t.text :preuse_equipment_instructions
-      t.text :preuse_element_instructions
-      t.text :preuse_environment_instructions
+      t.text :setup_equipment_instructions
+      t.text :setup_element_instructions
+      t.text :setup_environment_instructions
+      t.text :takedown_equipment_instructions
+      t.text :takedown_element_instructions
+      t.text :takedown_environment_instructions
       t.text :periodic_equipment_instructions
       t.text :periodic_element_instructions
       t.text :periodic_environment_instructions
