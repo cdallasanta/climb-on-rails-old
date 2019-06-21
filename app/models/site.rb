1ad31e10c38a108b1279a2a0eb4d23612730dfdb
chris.dallasanta <chris.dallasanta@gmail.com>
2019-06-21 15:59:05 -0700

    added recent comments by site

--- app/models/site.rb
+++ app/models/site.rb
@@ -5,9 +5,6 @@ class Site < ApplicationRecord
   validates_presence_of :name
 
   def self.recent_comments(site)
-    # Comment.where do |comment|
-    #   comment.commentable.element.site == site && comment.created_at < 1.week.ago.utc
-    # end
     Comment.where("created_at >= ?", 1.week.ago.utc).order(:element)
   end
 end
