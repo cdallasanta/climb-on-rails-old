c1cf34d5cdb85e5a6aa9db2e66c5b7e6195b1917
chris.dallasanta <chris.dallasanta@gmail.com>
2019-06-21 13:16:25 -0700

    added a class to meet project requirements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,7 @@ module ApplicationHelper
   # adds form errors to the top of the form
   def errors_check(object)
     if object.errors.any?
-      render partial: 'application/errors', locals: {object: object}, class: "alert alert-error"
+      render partial: 'application/errors', locals: {object: object}, class: "alert alert-error fields_with_errors"
     end
   end
 end
