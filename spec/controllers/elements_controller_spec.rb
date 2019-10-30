58f45dcf06375c90aef38f5d3a7c5137aa655bc9
cdallasanta <chris.dallasanta@gmail.com>
2019-10-30 10:28:48 -0700

    removed spec folder

--- spec/controllers/elements_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ElementsController, type: :controller do
-
-end
--- spec/controllers/ropes_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe RopesController, type: :controller do
-
-end
