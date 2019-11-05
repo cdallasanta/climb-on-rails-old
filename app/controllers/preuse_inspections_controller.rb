b5a0ffba56f1447dfd31682509703626554e4d46
cdallasanta <chris.dallasanta@gmail.com>
2019-11-04 16:05:47 -0800

    redirecting to /new from /show

--- app/controllers/preuse_inspections_controller.rb
+++ app/controllers/preuse_inspections_controller.rb
@@ -3,26 +3,52 @@ class PreuseInspectionsController < ApplicationController
 
   # /elements/:element_id/preuse_inspections/new
   def new
-    binding.pry
     params[:date] ? date = params[:date] : date = Date.today.to_s
-    @inspection = PreuseInspection.find_or_create_past_inspection(date, @element.id)
+    @inspection = PreuseInspection.find_or_init_past_inspection(date, @element.id)
     @inspection.setup = PreuseInspection::Setup.create unless @inspection.setup
-  end
     
+    # The following line, for some reason, makes it so the "Preuse Inspection must exist error"
+    # doesn't show up in the form. I don't know why yet
+    @inspection.valid?
+  end
+
+  # /elements/:element_id/preuse_inspections/:id
   def create
-    @inspection = @element.find_or_create_todays_inspection
-    @inspection.setup = PreuseInspection::Setup.create unless @inspection.setup
+    #updating preuse (just the date, really)
+    @inspection.date = preuse_params[:date]
+    #save preuse for date validation, to ensure the date is unique on that element
+    unless @inspection.save
+      link_to_previous_preuse_on_that_date and return
+    end
+
+    remove_empty_comments
+    #updating setup
+    setup = PreuseInspection::Setup.find_by(id: preuse_params[:setup_attributes][:id])
+    if setup == @inspection.setup
+      # if the inspection will change when saved, add the current user to be referenced by
+      # 'edited by', and also reduced the number of calls to the db
+      setup.assign_attributes(preuse_params[:setup_attributes])
+      if setup.changed_for_autosave?
+        setup.save
+        setup.users << current_user unless setup.users.include?(current_user)
+        flash[:alert] = "Inspection logged successfully"
+      end
+    else
+      flash[:alert] = "Setup inspection id not found"
+      render :edit and return
+    end
 
     redirect_to edit_element_preuse_inspection_path(@element, @inspection)
   end
 
+  # from datepicker JS
+  # /elements/:element_id/preuse_inspections/:date
   def show
-    @inspection = PreuseInspection.find_or_create_past_inspection(params[:date], params[:element_id])
+    @inspection = PreuseInspection.find_or_init_past_inspection(params[:date], params[:element_id])
     if @inspection.id != nil
-      render :js => "window.location = '#{edit_element_preuse_inspection_url(@inspection.element, @inspection)}'"
+      render js: "window.location = '#{edit_element_preuse_inspection_url(@inspection.element, @inspection)}'"
     else
-      binding.pry
-      render :js => "window.location = '#{new_element_preuse_inspection_url(@inspection.element)}"
+      render js: "window.location = '#{new_element_preuse_inspection_url(@inspection.element)}?date=#{params[:date]}'"
     end
   end
 
@@ -151,7 +177,7 @@ class PreuseInspectionsController < ApplicationController
   # an inspection logged
   # TODO: this might be unnecessary now
   def link_to_previous_preuse_on_that_date
-    previous_inspection = PreuseInspection.find_or_create_past_inspection(params[:preuse_inspection][:date], @element.id)
+    previous_inspection = PreuseInspection.find_or_init_past_inspection(params[:preuse_inspection][:date], @element.id)
     if previous_inspection
       flash[:alert] = "There is already an inspection logged for that date. View/edit it <a href='#{edit_element_preuse_inspection_path(@element, previous_inspection)}'>here</a>"
       redirect_to edit_element_preuse_inspection_path(@element, @inspection)
--- app/models/preuse_inspection.rb
+++ app/models/preuse_inspection.rb
@@ -13,7 +13,7 @@ class PreuseInspection < ApplicationRecord
     self.where(date:Date.today.strftime("%Y-%m-%d").to_s , element_id: element_id).first_or_create
   end
 
-  def self.find_or_create_past_inspection(date, element_id)
+  def self.find_or_init_past_inspection(date, element_id)
     self.find_or_initialize_by(date: Date.strptime(date, "%Y-%m-%d"), element_id: element_id)
   end
 end
