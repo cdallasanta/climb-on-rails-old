1a5483c60576ec088b28fece365c4e444842cd3f
cdallasanta <chris.dallasanta@gmail.com>
2019-11-06 11:07:30 -0800

    setting up users through active admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,28 +1,36 @@
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation
+  scope_to unless: proc{ current_user.fullname == "Chris Dalla Santa" } do
+    current_user.site
+  end
+  permit_params :fullname, :email, :role
+  config.sort_order = 'fullname_asc'
+  config.per_page = [10, 50, 100]
 
   index do
     selectable_column
-    id_column
+    column "Name", :fullname
     column :email
-    column :current_sign_in_at
-    column :sign_in_count
-    column :created_at
+    column :role
     actions
   end
 
+  filter :fullname, label: "Name"
   filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
-  filter :created_at
+  filter :role
 
   form do |f|
     f.inputs do
       f.input :email
-      f.input :password
-      f.input :password_confirmation
+      f.input :role, as: :select, collection: ["facilitator", "lead", "admin"]
     end
     f.actions
   end
-
+  
+  show title: :fullname do
+    attributes_table do
+      row('Name'){ |r| r.fullname }
+      row :email
+      row :role
+    end
+  end
 end
