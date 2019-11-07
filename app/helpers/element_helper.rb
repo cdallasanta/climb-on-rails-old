a98375424cb4e4c52382802c9f967ab2432a84cb
cdallasanta <chris.dallasanta@gmail.com>
2019-11-07 12:15:28 -0800

    removed html conversion from element helper

--- app/helpers/element_helper.rb
+++ app/helpers/element_helper.rb
@@ -1,9 +1,4 @@
 module ElementHelper
-  # used to convert the <br> stored in the db to a line break for the textarea
-  def html_to_string(text)
-    text.gsub("<br>","\n")
-  end
-
   # text field for each non-retired rope
   def ropes_fields(f)
     f.fields_for :ropes do |ff|
